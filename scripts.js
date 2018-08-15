e67bc012c15767a6b551f009ce28013e8163c781
vidit-agarwal <viditvivo@gmail.com>
2018-08-16 00:04:34 +0530

    Refractoring Code

--- scripts.js
+++ scripts.js
@@ -11,31 +11,35 @@ function flipCard() {
   {
       //first click
       hasFlippedCard = true ;
-      firstCard =this;
+      firstCard =this; 
+
+      return ;
   }
-  else {
-      //second click
-      hasFlippedCard = false;
-      secondCard = this;
+  
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
 
-      //do cards match here
-      if(firstCard.dataset.framework === secondCard.dataset.framework)
-      {
-          //its a match
-          firstCard.removeEventListener('click', flipCard) ;
-          secondCard.removeEventListener('click', flipCard) ;
-      }
-      else{
-          //not a match
-          setTimeout(() => {
-            firstCard.classList.remove('flip') ;
-            secondCard.classList.remove('flip') ;
-          } , 1000) ;
-       
-      }
-
-
-  }
+    checkForMatch() ;
 }
 
+function checkForMatch(){
+     //do cards match here
+     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework ;
+     
+     isMatch ? disableCards(): unFlipCards() ;
+     
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard) ;
+    secondCard.removeEventListener('click', flipCard) ;
+}
+
+function unFlipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip') ;
+        secondCard.classList.remove('flip') ;
+      } , 1000) ;
+}
 cards.forEach(card => card.addEventListener('click', flipCard));
